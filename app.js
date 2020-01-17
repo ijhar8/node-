52142a7cfe943f005221fe5ae3ea6bdc7512bc2a
ijhar <ijhar.a@boppotechnologies.com>
2020-01-17 18:12:15 +0530

    DB

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/****** Database connection *******/
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(`mongodb+srv://ijhar:1234@cluster0-hi3ct.mongodb.net/test?retryWrites=true&w=majority`);
+mongoose.connection.on('error', (err) => {
+    // console.error(err);
+    console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+    process.exit();
+});
 
 
 app.get('/', (req, res) => {
