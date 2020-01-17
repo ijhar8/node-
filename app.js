b0b6d2e5bb900ed5944b29927188e6e79c9a188c
ijhar <ijhar.a@boppotechnologies.com>
2020-01-17 17:55:01 +0530

    f

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', (req, res) => {
 
     return res.send(JSON.stringify({ "lol": "lol" }))
 })
+
+
 
 
 app.listen(8080, () => console.log(`Example app listening on port ${8080}!`));
